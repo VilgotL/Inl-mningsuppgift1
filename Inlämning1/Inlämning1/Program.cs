7f8d6a8c8660c10e7da87aafb95ac204fe69b88d
VilgotL <54435285+VilgotL@users.noreply.github.com>
2019-09-13 09:43:50 +0200

    Update Program.cs

--- Inlämning1/Inlämning1/Program.cs
+++ Inlämning1/Inlämning1/Program.cs
@@ -1,14 +1,29 @@
-﻿using System;
+using System;
 
-namespace Inlämning1
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("hello");
-            Console.WriteLine("Hej");
-        }
+            Random rnd = new Random();
+            int Tal = rnd.Next(1, 101);
+            int AntalGissningar;
+            int Gissning = 0;
+            Console.WriteLine("Gissa talet: ");
+            for (AntalGissningar = 0; Gissning != Tal; AntalGissningar++)
+            {
+                Gissning = int.Parse(Console.ReadLine());
+                if (Gissning > Tal)
+                {
+                    Console.WriteLine("Du gissade för högt.");
+                }
+                else if (Gissning < Tal)
+                {
+                    Console.WriteLine("Du gissade för lågt.");
+                }
+            }
+            Console.WriteLine("Du gissade rätt efter "+AntalGissningar+" försök.");
+        }   
     }
 }
